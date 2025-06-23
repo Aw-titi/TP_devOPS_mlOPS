7c71abacd4d22bb60a0f3f3bee534023c0117a19
angelique_wahba <angeliquewahba@icloud.com>
2025-06-23 22:56:06 +0200

    First commit

--- run_all.sh
+++ run_all.sh
@@ -1,18 +1,18 @@
 set -e
 
-echo "Commencement du projet devops, mlops"
+echo "Commencement"
 
 echo "Terraform"
 cd devops/terraform
 terraform init
 terraform plan
-terraform apply 
+terraform apply -auto-approve
 
 EC2_IP=$(terraform output -raw public_ip)
 echo "Adresse IP publique de l'EC2 : $EC2_IP"
-cd ..
+cd ../..
 
-echo "Mise à jour inventory.ini"
+echo "Mise à jour de inventory.ini"
 cat > devops/ansible/inventory.ini <<EOF
 [webservers]
 ec2-instance-1 ansible_host=$EC2_IP
@@ -24,30 +24,31 @@ ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 ansible_python_interpreter=/usr/bin/python3
 EOF
 
-echo "Ansible"
+echo "Playbook Ansible"
 cd devops/ansible
 ansible-playbook -i inventory.ini playbook.yml
-cd ..
+cd ../..
 
-echo "Transfert ML et API vers l’EC2"
-scp -i labsuser.pem -r ml ubuntu@$EC2_IP:~/
-scp -i labsuser.pem -r api ubuntu@$EC2_IP:~/
+echo "Transfère ML et API"
+scp -i ~/.ssh/labsuser.pem -r ml ubuntu@$EC2_IP:~/
+scp -i ~/.ssh/labsuser.pem -r api ubuntu@$EC2_IP:~/
 
 echo "ML"
-ssh -i labsuser.pem ubuntu@$EC2_IP <<EOF
+ssh -i ~/.ssh/labsuser.pem ubuntu@$EC2_IP <<EOF
   cd ml
   docker build -t mlapp .
-  docker run -d --name ml_container -p 5000:5000 mlapp
+  docker run -d --name ml_container -p 8000:8000 mlapp
 EOF
 
 echo "API"
-ssh -i labsuser.pem ubuntu@$EC2_IP <<EOF
+ssh -i ~/.ssh/labsuser.pem ubuntu@$EC2_IP <<EOF
   cd api
   docker build -t apiapp .
   docker run -d --name api_container -p 3000:3000 apiapp
 EOF
 
 echo ""
-echo "Terminé !"
+echo "Déploiement terminé."
 echo "API: http://$EC2_IP:3000"
-echo "ML: http://$EC2_IP:5000/predict"
+echo "ML: http://$EC2_IP:8000/predict"
+
